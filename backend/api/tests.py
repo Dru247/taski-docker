368ff0dc33438ced005328c6252d0976ed63c877
Dru <lehtin.aa@mail.ru>
2025-12-10 18:46:00 +0300

    Добавлены тесты.

---
+++ backend/api/tests.py
@@ -0,0 +1,24 @@
+"""Тесты API."""
+from http import HTTPStatus
+
+from api.models import Task
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    """Класс тестирования API."""
+
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(Task.objects.filter(title='Test').exists())
